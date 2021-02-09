eaab5ca31a22b40a364001603edd06d397083e4a
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-02-09 20:20:53 +0700

    Updates

---
+++ static/js/7.c72fdc7e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprawg-client"]=this["webpackJsonprawg-client"]||[]).push([[7],{161:function(t,c,e){"use strict";e.r(c);var n=e(2),a=e(52),i=e(53),b=e(73),r=e(96);c.default=Object(i.b)((function(t){return Object(n.a)(Object(n.a)({},Object(r.h)(t)),{},{login:b.f})}))(a.f)}}]);
+//# sourceMappingURL=7.c72fdc7e.chunk.js.map
---
+++ static/js/8.10ebe5eb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprawg-client"]=this["webpackJsonprawg-client"]||[]).push([[8],{164:function(e,n,a){"use strict";a.r(n);var t=a(1),l=a.n(t),c=a(160);n.default=function(){return l.a.createElement("div",{className:"not-found"},l.a.createElement("div",{className:"not-found__404"},l.a.createElement("span",null,"4"),l.a.createElement(c.a,null),l.a.createElement("span",null,"4")),l.a.createElement("span",{className:"not-found__title"},"Page not found"))}}}]);
+//# sourceMappingURL=8.10ebe5eb.chunk.js.map
