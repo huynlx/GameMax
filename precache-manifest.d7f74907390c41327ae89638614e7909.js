f4cc01685fe73e6c19299abe7f7ace0637e5bc3b
Lê Xuân Huynh <huynhlx62@wru.vn>
2021-02-09 21:12:15 +0700

    Updates

---
+++ precache-manifest.d7f74907390c41327ae89638614e7909.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd94d17ecf169479a5242c574f589b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160efbcf51069d179198",
+    "url": "/static/css/3.3a221eca.chunk.css"
+  },
+  {
+    "revision": "c81b91962091d85ea5b4",
+    "url": "/static/css/main.50c5be04.chunk.css"
+  },
+  {
+    "revision": "07cfbdcbf357c562324f",
+    "url": "/static/js/0.91648380.chunk.js"
+  },
+  {
+    "revision": "75c10d62f5882d103abe",
+    "url": "/static/js/10.0dd9c04f.chunk.js"
+  },
+  {
+    "revision": "6d8cacd25f6d140a7f1a",
+    "url": "/static/js/11.3fa15ec3.chunk.js"
+  },
+  {
+    "revision": "160efbcf51069d179198",
+    "url": "/static/js/3.9088e450.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/static/js/3.9088e450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6625a66698006ace242",
+    "url": "/static/js/4.3f25040b.chunk.js"
+  },
+  {
+    "revision": "997f1a95acaa471a6b60",
+    "url": "/static/js/5.d1097a49.chunk.js"
+  },
+  {
+    "revision": "f73767d6cea64e89e13e",
+    "url": "/static/js/6.337358ad.chunk.js"
+  },
+  {
+    "revision": "3114caf201730a46df1d",
+    "url": "/static/js/7.c72fdc7e.chunk.js"
+  },
+  {
+    "revision": "dc49788f8df761d04ed2",
+    "url": "/static/js/8.10ebe5eb.chunk.js"
+  },
+  {
+    "revision": "24636eda05a33adc3bba",
+    "url": "/static/js/9.ea366f84.chunk.js"
+  },
+  {
+    "revision": "c81b91962091d85ea5b4",
+    "url": "/static/js/main.8eb3f827.chunk.js"
+  },
+  {
+    "revision": "fdfbd4315da3ab8c718b",
+    "url": "/static/js/runtime-main.dc71c9ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Game/precache-manifest.f349cf192d00df8030dde706fc0d1713.js"
+  "/precache-manifest.d7f74907390c41327ae89638614e7909.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
